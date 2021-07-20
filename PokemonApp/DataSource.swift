002f73ba1bc3fd40722513ea47a391744c590d22
null-miraiLABO <mizuki2002b@gmail.com>
2021-07-20 15:43:39 +0900

    2th commit

--- PokemonApp/DataSource.swift
+++ PokemonApp/DataSource.swift
@@ -8,14 +8,15 @@
 import Combine
 import SwiftUI
 
-class dataSource: ObservableObject {
+class DataSource: ObservableObject {
     var pokemon = [PokemonData]()
     
     init() {
         pokemon = [
             PokemonData(name: "name1", type: "type1", imgName: "07", data: "data1"),
-            PokemonData(name: "name2", type: "type2", imgName: "06", data: "data2")
+            PokemonData(name: "name2", type: "type2", imgName: "06", data: "data2"),
+            PokemonData(name: "name3", type: "type3", imgName: "05", data: "data3"),
+            PokemonData(name: "name4", type: "type4", imgName: "04", data: "data4")
         ]
     }
-    
 }
---
+++ PokemonApp/ListView.swift
@@ -0,0 +1,29 @@
+//
+//  ListView.swift
+//  PokemonApp
+//
+//  Created by 岡瀬　瑞樹 on 2021/07/20.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    @ObservedObject var dataSource = DataSource()
+    
+    var body: some View {
+        NavigationView{
+            List(0 ..< dataSource.pokemon.count){
+                item in RowView(pokemon: dataSource.pokemon[item])
+            }
+            .navigationBarTitle("ポケモン図鑑")
+        }
+    }
+}
+
+#if DEBUG
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
+#endif
