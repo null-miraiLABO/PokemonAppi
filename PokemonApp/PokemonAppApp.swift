999f57ddcd6e90d2ef86488e89fa59b3a1750963
null-miraiLABO <mizuki2002b@gmail.com>
2021-07-20 13:32:35 +0900

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by 岡瀬　瑞樹 on 2021/07/20.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
